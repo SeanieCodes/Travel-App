ed2b5213c658c1c96b023295a4157934ecf2152b
SeanieCodes <seanhello12321@gmail.com>
2024-12-19 14:30:26 +0800

    feat: added city search and city card components

---
+++ frontend/travelapp/src/components/CityCard/CityCard.jsx
@@ -0,0 +1,5 @@
+const CityCard = () => {
+    return <h1>City Card Component</h1>;
+};
+
+export default CityCard;
---
+++ frontend/travelapp/src/components/CitySearch/CitySearch.jsx
@@ -0,0 +1,5 @@
+const CitySearch = () => {
+    return <h1>City Search Component</h1>;
+};
+
+export default CitySearch;
