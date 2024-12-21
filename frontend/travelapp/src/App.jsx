2856ecc246ad0aae43699ac24a2dcf4c7067b993
SeanieCodes <seanhello12321@gmail.com>
2024-12-21 15:44:46 +0800

    feat: put city search into calendar, changed routing to reflect changes

--- frontend/travelapp/src/App.jsx
+++ frontend/travelapp/src/App.jsx
@@ -1,8 +1,6 @@
-import './App.css'
-import { useState } from 'react'
+import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Calendar from './components/Calendar/Calendar';
-import CitySearch from './components/CitySearch/CitySearch';
 import CityCard from './components/CityCard/CityCard';
 
 const App = () => {
@@ -11,14 +9,11 @@ const App = () => {
             <nav>
                 <ul>
                     <li><Link to="/">Calendar</Link></li>
-                    <li><Link to="/city-search">City Search</Link></li>
                     <li><Link to="/city-card">City Card</Link></li>
                 </ul>
             </nav>
-
             <Routes>
                 <Route path="/" element={<Calendar />} />
-                <Route path="/city-search" element={<CitySearch />} />
                 <Route path="/city-card" element={<CityCard />} />
             </Routes>
         </Router>
--- frontend/travelapp/src/components/Calendar/Calendar.jsx
+++ frontend/travelapp/src/components/Calendar/Calendar.jsx
@@ -1,15 +1,21 @@
-import {useState} from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import { useState } from "react";
+import Calendar from "react-calendar";
+import CitySearch from "../CitySearch/CitySearch";
+import "react-calendar/dist/Calendar.css";
+import "./Calendar.css";
 
 const MyCalendar = () => {
     const [date, setDate] = useState(new Date());
 
     return (
-        <div className="app">
-            <Calendar onChange={setDate} value={date} />
+        <div className="calendar-page">
+            <CitySearch />
+            <div className="calendar-container">
+                <h1>Your Travel Calendar</h1>
+                <Calendar onChange={setDate} value={date} />
+            </div>
         </div>
     );
-}
+};
 
 export default MyCalendar;
