319626096d535df0c6951da82818db58fdbcc65f
SeanieCodes <seanhello12321@gmail.com>
2024-12-21 15:58:13 +0800

    feat: modded routing so there aren't explicit nav buttons on calendar page

--- frontend/travelapp/src/App.jsx
+++ frontend/travelapp/src/App.jsx
@@ -1,17 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calendar from './components/Calendar/Calendar';
 import CityCard from './components/CityCard/CityCard';
 
 const App = () => {
     return (
         <Router>
-            <nav>
-                <ul>
-                    <li><Link to="/">Calendar</Link></li>
-                    <li><Link to="/city-card">City Card</Link></li>
-                </ul>
-            </nav>
             <Routes>
                 <Route path="/" element={<Calendar />} />
                 <Route path="/city-card" element={<CityCard />} />
