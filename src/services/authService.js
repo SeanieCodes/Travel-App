ed608e1dc8a22d145627ac2fb24f670d28765b15
SeanieCodes <seanhello12321@gmail.com>
2025-03-27 12:49:58 +0800

    refactor: change port

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';  // set up backend later
+const API_URL = 'http://localhost:5001/api';  // set up backend later
 
 export const signIn = async (credentials) => {
   try {
